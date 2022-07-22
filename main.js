600167933980f00ab3d25cdc56571e8617bc4328
hai2086 <haitn2806@gmail.com>
2022-07-22 15:40:34 +0700

    homepage

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+// const { createLogger } = require("vite");
+console.log("ahiiihi");
+const card = document.querySelector(".card");
+const movement = card.getBoundingClientRect().width; // get width m
+console.log(movement);
+console.log(card);
 let defaultTransform = 0;
 function goNext() {
-  defaultTransform = defaultTransform - 398;
+  console.log(movement);
+  defaultTransform = defaultTransform - movement;
   var slider = document.getElementById("slider");
   if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
     defaultTransform = 0;
@@ -10,7 +17,7 @@ next.addEventListener("click", goNext);
 function goPrev() {
   var slider = document.getElementById("slider");
   if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-  else defaultTransform = defaultTransform + 398;
+  else defaultTransform = defaultTransform + movement;
   slider.style.transform = "translateX(" + defaultTransform + "px)";
 }
 prev.addEventListener("click", goPrev);
